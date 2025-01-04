106982202c49d92cd0bcd5ce835e29d566ef5c12
sergey <reunok2000@gmail.com>
2025-01-04 03:03:39 +0300

    add babel, add extract plugin

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,24 @@
-import { Link, Route, Routes } from "react-router-dom";
 import './styles/index.scss'
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
-import { Suspense } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
+import {Navbar} from "wigets/Navbar";
+import {Sidebar} from "wigets/Sidebar";
+import {Suspense} from "react";
+import {useTranslation} from "react-i18next";
 
 const App = () => {
-  const {theme, toggleTheme} = useTheme()
+    const {theme, toggleTheme} = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О сайте</Link>
-      <button onClick={toggleTheme}>Toggle theme</button>
-      <AppRouter />
+        <Suspense fallback="">
+            <Navbar/>
+            <div className="content-page">
+                <Sidebar/>
+                <AppRouter/>
+            </div>
+        </Suspense>
     </div>
   )
 }
